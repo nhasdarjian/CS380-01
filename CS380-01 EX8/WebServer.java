cceda72c35a815e92ba7f9f02f05fd6a717d242d
Nathan Asdarjian <nathanasdarjian@gmail.com>
2017-12-01 03:06:22 -0800

    Added 404 page

--- CS380-01 EX8/WebServer.java
+++ CS380-01 EX8/WebServer.java
@@ -61,13 +61,33 @@ public class WebServer
 						String response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + f.length() + "\r\n";
 						pw.println(response);
 						System.out.print(response);
-						BufferedReader fbr = new BufferedReader(new FileReader(f));
-						while ((temp = fbr.readLine()) != null)
-						{
-							pw.println(temp);
-							System.out.print(temp + "\r\n");
-						}
+					} 
+					else
+					{
+						f = new File("www/error.html");
+						String response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: " + f.length() + "\r\n";
+						pw.println(response);
+						System.out.print(response);
 					}
+					
+					BufferedReader fbr = new BufferedReader(new FileReader(f));
+					while ((temp = fbr.readLine()) != null)
+					{
+						pw.println(temp);
+						System.out.print(temp + "\r\n");
+					}
+					
+					/*
+					else
+					{
+						String response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: 126\r\n";
+						pw.println(response);
+						System.out.print(response);
+						response = "<html>\r\n<head>\r\n<title>Not Found</title>\r\n</head>\r\n<body>Sorry, the object you requested was not found.</body>\r\n</html>";
+						pw.println(response);
+						System.out.println(response);
+					}
+					*/
 				}
 			}
 			catch (IOException e)
